15eff8b7b04f8be0e672bb0d8d963eccee9d326e
marina-fdz <marina.fdz.caceres@gmail.comgit config --global core.editor code>
2024-05-21 15:37:44 +0200

    tryout

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/promo-X-module-3-pai-7-grogu-game/",
+  base: "/promo-X-module-3-pair-7-grogu-game/",
 })
